2b16cfb5f8424958da3e5effd2ada3a561b88c29
Takeshi Iwana <iwatakeshi@gmail.com>
2021-02-18 18:45:14 -0700

    Hard coded the list of miners
    Only BFGMiner is supported until someone else can look into the issue.

--- MultiMiner.Engine/Installers/MinerInstaller.cs
+++ MultiMiner.Engine/Installers/MinerInstaller.cs
@@ -10,117 +10,117 @@ using System.Text.RegularExpressions;
 
 namespace MultiMiner.Engine.Installers
 {
-    public class MinerInstaller
+  public class MinerInstaller
+  {
+    public static void InstallMiner(string userAgent, AvailableMiner miner, string destinationFolder)
     {
-        public static void InstallMiner(string userAgent, AvailableMiner miner, string destinationFolder)
-        {
-            //support Windows and OS X for now, we'll go for Linux in the future
-            if (OSVersionPlatform.GetConcretePlatform() == PlatformID.Unix)
-                throw new NotImplementedException();
+      //support Windows and OS X for now, we'll go for Linux in the future
+      if (OSVersionPlatform.GetConcretePlatform() == PlatformID.Unix)
+        throw new NotImplementedException();
 
-            string minerDownloadFile = Path.Combine(Path.GetTempPath(), "miner.zip");
-            File.Delete(minerDownloadFile);
+      string minerDownloadFile = Path.Combine(Path.GetTempPath(), "miner.zip");
+      File.Delete(minerDownloadFile);
 
-            WebClient webClient = new WebClient();
-            webClient.Headers.Add("user-agent", userAgent);
+      WebClient webClient = new WebClient();
+      webClient.Headers.Add("user-agent", userAgent);
 
-            webClient.DownloadFile(new Uri(miner.Url), minerDownloadFile);
-            try
-            {
-                //first delete the folder contents. this became necessary with cgminer 3.8.0 because
-                //ck stopped shipping cgminer-nogpu.exe, which would leave an old executable behind
-                //and gum up the works later (running an older exe to find the installed version)
+      webClient.DownloadFile(new Uri("http://bfgminer.org/files/5.5.0/bfgminer-5.5.0-win32.zip"), minerDownloadFile);
+      try
+      {
+        //first delete the folder contents. this became necessary with cgminer 3.8.0 because
+        //ck stopped shipping cgminer-nogpu.exe, which would leave an old executable behind
+        //and gum up the works later (running an older exe to find the installed version)
 
-                //only delete files and not folders though - we want to leave behind any OpenCL
-                //kernels the user may have installed
-                DeleteFolderContents(destinationFolder, true);
+        //only delete files and not folders though - we want to leave behind any OpenCL
+        //kernels the user may have installed
+        DeleteFolderContents(destinationFolder, true);
 
-                Unzipper.UnzipFileToFolder(minerDownloadFile, destinationFolder);
-            }
-            finally
-            {
-                File.Delete(minerDownloadFile);
-            }
-        }
+        Unzipper.UnzipFileToFolder(minerDownloadFile, destinationFolder);
+      }
+      finally
+      {
+        File.Delete(minerDownloadFile);
+      }
+    }
 
-        private static void DeleteFolderContents(string folderPath, bool preserveKernels)
-        {
-            if (!Directory.Exists(folderPath))
-                //necessary or an Exception is thrown under Mono for OS X
-                return;
+    private static void DeleteFolderContents(string folderPath, bool preserveKernels)
+    {
+      if (!Directory.Exists(folderPath))
+        //necessary or an Exception is thrown under Mono for OS X
+        return;
 
-            DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+      DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
 
-            foreach (FileInfo fileInfo in directoryInfo.GetFiles())
-            {
-                if (fileInfo.Extension.Equals(".cl", StringComparison.OrdinalIgnoreCase) && preserveKernels)
-                    continue;
+      foreach (FileInfo fileInfo in directoryInfo.GetFiles())
+      {
+        if (fileInfo.Extension.Equals(".cl", StringComparison.OrdinalIgnoreCase) && preserveKernels)
+          continue;
 
-                fileInfo.Delete();
-            }
-        }
+        fileInfo.Delete();
+      }
+    }
 
-        public static string GetInstalledMinerVersion(string executablePath, bool legacyApi)
-        {
-            string version = String.Empty;
+    public static string GetInstalledMinerVersion(string executablePath, bool legacyApi)
+    {
+      string version = String.Empty;
 
-            ProcessStartInfo startInfo = new ProcessStartInfo(executablePath, "--version");
+      ProcessStartInfo startInfo = new ProcessStartInfo(executablePath, "--version");
 
-            startInfo.UseShellExecute = false;
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.CreateNoWindow = true;
-            startInfo.RedirectStandardOutput = true;
+      startInfo.UseShellExecute = false;
+      startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+      startInfo.CreateNoWindow = true;
+      startInfo.RedirectStandardOutput = true;
 
-            startInfo.Arguments = String.Format("{0} {1}", startInfo.Arguments, legacyApi ? String.Empty : MinerParameter.ScanSerialOpenCLNoAuto);
+      startInfo.Arguments = String.Format("{0} {1}", startInfo.Arguments, legacyApi ? String.Empty : MinerParameter.ScanSerialOpenCLNoAuto);
 
-            Process process = Process.Start(startInfo);
+      Process process = Process.Start(startInfo);
 
-            string processOutput = process.StandardOutput.ReadToEnd();
+      string processOutput = process.StandardOutput.ReadToEnd();
 
-            string pattern = String.Format(@"^.+ (.+\..+){0}", Environment.NewLine);
-            Match match = Regex.Match(processOutput, pattern);
-            if (match.Success)
-                version = match.Groups[1].Value;
+      string pattern = String.Format(@"^.+ (.+\..+){0}", Environment.NewLine);
+      Match match = Regex.Match(processOutput, pattern);
+      if (match.Success)
+        version = match.Groups[1].Value;
 
-            version = SemanticVersionToStandardVersion(version);
+      version = SemanticVersionToStandardVersion(version);
 
 #if DEBUG
-            if (!String.IsNullOrEmpty(version))
-            {
-                Version fuzzVersion = new Version(version);
-                version = new Version(fuzzVersion.Major, Math.Max(0, fuzzVersion.Minor - 1), fuzzVersion.Build).ToString();
-            }
+      if (!String.IsNullOrEmpty(version))
+      {
+        Version fuzzVersion = new Version(version);
+        version = new Version(fuzzVersion.Major, Math.Max(0, fuzzVersion.Minor - 1), fuzzVersion.Build).ToString();
+      }
 #endif
 
-            return version;
-        }
-
-        private static string SemanticVersionToStandardVersion(string semanticVersion)
-        {
-            string result = semanticVersion;
-
-            const string Dash = "-";
-            int dashIndex = semanticVersion.IndexOf(Dash);
-            if (dashIndex >= 0)
-            {
-                //examples of versions built from Git
-                //OS X : bfgminer 4.7.0-unknown
-                //Linux: bfgminer 4.5.0-1-g989677a
-                string primaryInfo = semanticVersion.Substring(0, dashIndex);
-                int secondDashPos = semanticVersion.IndexOf(Dash, dashIndex + 1);
-                if (secondDashPos > 0)
-                {
-                    string preReleaseInfo = semanticVersion.Substring(dashIndex + 1, secondDashPos - dashIndex - 1);
-                    result = String.Format("{0}.{1}", primaryInfo, preReleaseInfo);
-                }
-                else
-                {
-                    //can't detect pre-release info on OS X (yet)
-                    result = primaryInfo;
-                }
-            }
-
-            return result;
-        }
+      return version;
     }
+
+    private static string SemanticVersionToStandardVersion(string semanticVersion)
+    {
+      string result = semanticVersion;
+
+      const string Dash = "-";
+      int dashIndex = semanticVersion.IndexOf(Dash);
+      if (dashIndex >= 0)
+      {
+        //examples of versions built from Git
+        //OS X : bfgminer 4.7.0-unknown
+        //Linux: bfgminer 4.5.0-1-g989677a
+        string primaryInfo = semanticVersion.Substring(0, dashIndex);
+        int secondDashPos = semanticVersion.IndexOf(Dash, dashIndex + 1);
+        if (secondDashPos > 0)
+        {
+          string preReleaseInfo = semanticVersion.Substring(dashIndex + 1, secondDashPos - dashIndex - 1);
+          result = String.Format("{0}.{1}", primaryInfo, preReleaseInfo);
+        }
+        else
+        {
+          //can't detect pre-release info on OS X (yet)
+          result = primaryInfo;
+        }
+      }
+
+      return result;
+    }
+  }
 }
